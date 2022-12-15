de4ac0c1a8b919be0e3daf596cad6f3e37aa2167
cheanchharakvily <chharakvily@gmail.com>
2022-12-15 21:32:46 +0700

    implement add function

--- gui.py
+++ gui.py
@@ -1,10 +1,25 @@
 from Functions import openfileread, openfilewrite
-import PySimpleGUI
+import PySimpleGUI as gui
 
-label = PySimpleGUI.Text("Enter your Text")
-user_input = PySimpleGUI.InputText()
-add_button = PySimpleGUI.Button('Add')
+label = gui.Text("Enter your Text")
+user_input = gui.InputText(tooltip="Enter todo", key="todo")
+add_button = gui.Button('Add')
 
-window = PySimpleGUI.Window('To do App', layout=[[label], [user_input, add_button]])
-window.read()
-window.close()
+window = gui.Window('To do App',
+                            layout=[[label], [user_input, add_button]],
+                            font=('Helvetica', 11))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = openfileread()
+            new_todos = values['todo'] + "\n"
+            todos.append(new_todos)
+            openfilewrite(todos)
+        case gui.WIN_CLOSED:
+            break
+
+    window.close()
